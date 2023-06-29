2d7300a8b5b92df1a0a3e80058d0869009fcb97f
sabatohura <sabato@hura.one>
2023-06-29 04:29:32 +0200

    Create User create Mutation and Test

--- graphql_app/models.py
+++ graphql_app/models.py
@@ -6,4 +6,6 @@ class User(models.Model):
     name = models.CharField(max_length=250)
     followers = models.ManyToManyField("graphql_app.User")
 
-    
+class Post(models.Model):
+    content = models.TextField()
+    created_by = models.ForeignKey("graphql_app.User", on_delete=models.DO_NOTHING)
--- graphql_app/tests.py
+++ graphql_app/tests.py
@@ -33,4 +33,76 @@ class GraphQLTest(GraphQLTestCase):
         )
         self.assertEqual(res.status_code, 200)
         self.assertEqual(expected, res.json())
+
+    # def test_retrieve_user_with_posts(self):
+    #     expected = {
+    #         "data": {
+    #             "user": {
+    #                 "id": "2",
+    #                 "name": "Dan Bilzerian",
+    #                 "followers": [
+    #                     {
+    #                         "name": "John Doe"
+    #                     }
+    #                 ],
+    #                 "postSet": [
+    #                     {
+    #                         "content": "I love to party!!!"
+    #                     }
+    #                 ]
+    #             }
+    #         }
+    #     }
+
+    #     res = self.query(
+    #         """
+    #         {
+    #           user(id: 2) {
+    #             id
+    #             name
+    #             followers {
+    #               name
+    #             }
+    #             postSet {
+    #               content
+    #             }
+    #           }
+    #         }
+    #         """
+    #     )
+    #     self.assertEqual(res.status_code, 200)
+    #     self.assertEqual(expected, res.json())
+
+    def test_create_user(self):
+         expected = {
+            "data": {
+                "createUser":{
+                "ok":True,
+                "user": {
+                    "id": "3",
+                    "name": "Elon Musk"
+                }
+                }
+            }
+        }
+         
+         res = self.query(
+             """
+             mutation createUser{
+                createUser(input:{
+                    name:"Elon Musk"
+                }) {
+                ok
+                user {
+                    id
+                    name
+                }
+                }
+             }
+             """
+         )
+         self.assertEqual(res.status_code, 200)
+         self.assertEqual(expected, res.json())
+         
+
 # Create your tests here.
